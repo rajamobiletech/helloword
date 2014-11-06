c44c2f13ee64c3bad99194d972f0e6d24a6a4202
Amsha <hcl@hcls-Mac-mini-2.local>
2014-11-06 12:02:50 +0530

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Amsha on 06/11/14.
+//  Copyright (c) 2014 Amsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Amsha on 06/11/14.
+//  Copyright (c) 2014 Amsha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
